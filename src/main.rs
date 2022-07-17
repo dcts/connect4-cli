ad354432665e3e6918291dd66742dedd5614a0a7
dcts <thomas@liist.com>
2022-07-17 05:51:45 +0200

    :construction: not yet working

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,66 @@
+
+use rand::Rng;
+
 fn main() {
     println!("Connect 4!");
+    let board = Board::new();
+    board.print();
 }
 
 // BOARD.rs
 struct Board {
-    matrix: Vec<i32>
+    slots: [SlotState; 7*6] // 7 cols * 6 rows
 }
 
+impl Board {
+    fn new() -> Board {
+        Board {
+            slots: [SlotState::Empty; 7*6]
+        }
+    }
+    fn random() -> () {
+        let sss: [i32; 7*6];
+        println!("{}", "hello".to_string());
+        // let mut c = 0;
+        // while c < 7*6 {
+        //     slots[c] = match rand::thread_rng().gen_range(0..=2) {
+        //         0 => SlotState::Empty,
+        //         1 => SlotState::Occupied(Player::One),
+        //         2 => SlotState::Occupied(Player::Two),
+        //         _ => panic!("Randomly generated value is out of bound. Allowed 0-2."),
+        //     };
+        //     c += 1;
+        // }
+        // Board { 
+        //     slots: slots,
+        // }
+    }
+
+    fn print(&self) {
+        let mut c = 0;
+        for slot in self.slots {
+            match slot {
+                SlotState::Empty => print!("  |"),
+                SlotState::Occupied(p) => match p {
+                    Player::One => print!("🔴|"),
+                    Player::Two => print!("🟡|"),
+                }
+            }
+            if (c+1) % 7 == 0 {
+                println!("");
+            }
+            c += 1;
+        }
+    }
+}
+
+#[derive(Copy, Clone, Debug)]
 enum SlotState {
     Empty,
     Occupied(Player)
 }
 
+#[derive(Copy, Clone, Debug)]
 enum Player {
     One,
     Two
@@ -25,4 +74,16 @@ enum Column {
     Five,
     Six,
     Seven
+}
+
+struct Position {
+    row: i8,
+    col: i8,
+}
+
+enum Direction {
+    Left,
+    Right,
+    Up,
+    Down,
 }
