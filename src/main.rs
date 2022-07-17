be6492d7a5447801b5502e395d86fadd452970cb
dcts <thomas@liist.com>
2022-07-17 18:54:25 +0200

    :sparkle: add demo for game logic back in

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,21 @@
 // mod board;
 mod network;
-
 use network::server::GameSocket;
-// use board::Board;
+
+mod board;
+use board::Board;
 
 // fn game_callback(s: String) -> String {
 
 // }
 // sudo lsof -i -P | grep LISTEN | grep :3042
 fn main() {
-    // start
+    // game logic demo
+    println!("Connect 4!");
+    let board = Board::random();
+    board.print();
+
+    // networking demo
     let connection_string = String::from("0.0.0.0:3042");
     // let socket = GameSocket::new(connection_string);
 }
